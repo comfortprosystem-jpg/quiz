3d0de4d111b66b4fd4d8fb28ce22469e71b2e0ef
comfortprosystem-jpg <comfortprosystem@gmail.com>
2025-08-12 16:04:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ function nextStep(step) {
 function selectStyle(style) {
     selectedStyle = style;
     selectOption(event, 'step-2');
-    document.body.style.backgroundColor = colorMap[style]; // Меняем только цвет
+    // Убираем строку, которая меняла фон, чтобы он оставался статичным
     
     const mechanismOptionsDiv = document.getElementById('mechanism-options');
     mechanismOptionsDiv.innerHTML = ''; 
